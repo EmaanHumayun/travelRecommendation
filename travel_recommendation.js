078c446af437cd9accf5b46b5f23391ff88ed03e
EmaanHumayun <emaanhumayun23072@gmail.com>
2026-09-19 16:15:29 +0500

    Complete travel recommendation website structure

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,1 +1,8 @@
-console.log("Travel Recommendation Website");
+fetch("travel_recommendation_api.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error("Error fetching travel data:", error);
+    });
